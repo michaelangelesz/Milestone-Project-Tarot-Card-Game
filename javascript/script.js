e694a5d1baecf0ecc23bc3c518877f9022af19de
michaelangelesz <125311967+michaelangelesz@users.noreply.github.com>
2023-06-10 01:58:40 -0400

    add cards array to js

---
+++ javascript/script.js
@@ -0,0 +1,90 @@
+const majorArcana = [
+  {
+    cardName: "The Fool",
+    cardImage: "images/majorArcana/0_theFool.jpg",
+  },
+  {
+    cardName: "The Magician",
+    cardImage: "images/majorArcana/1_theMagician.jpg",
+  },
+  {
+    cardName: "The High Priestess",
+    cardImage: "images/majorArcana/2_theHighPriestess.jpg",
+  },
+  {
+    cardName: "The Empress",
+    cardImage: "images/majorArcana/3_theEmpress.jpg",
+  },
+  {
+    cardName: "The Emperor",
+    cardImage: "images/majorArcana/4_theEmperor.jpg",
+  },
+  {
+    cardName: "The Hierophant",
+    cardImage: "images/majorArcana/5_theHierophant.jpg",
+  },
+  {
+    cardName: "The Lovers",
+    cardImage: "images/majorArcana/6_theLovers.jpg",
+  },
+  {
+    cardName: "The Chariot",
+    cardImage: "images/majorArcana/7_theChariot.jpg",
+  },
+  {
+    cardName: "Strength",
+    cardImage: "images/majorArcana/8_Strength.jpg",
+  },
+  {
+    cardName: "The Hermit",
+    cardImage: "images/majorArcana/9_theHermit.jpg",
+  },
+  {
+    cardName: "Wheel of Fortune",
+    cardImage: "images/majorArcana/10_wheelOfFortune.jpg",
+  },
+  {
+    cardName: "Justice",
+    cardImage: "images/majorArcana/11_Justice.jpg",
+  },
+  {
+    cardName: "The Hanged Man",
+    cardImage: "images/majorArcana/12_theHangedMan.jpg",
+  },
+  {
+    cardName: "Death",
+    cardImage: "images/majorArcana/13_Death.jpg",
+  },
+  {
+    cardName: "Temperance",
+    cardImage: "images/majorArcana/14_Temperance.jpg",
+  },
+  {
+    cardName: "The Devil",
+    cardImage: "images/majorArcana/15_theDevil.jpg",
+  },
+  {
+    cardName: "The Tower",
+    cardImage: "images/majorArcana/16_theTower.jpg",
+  },
+  {
+    cardName: "The Star",
+    cardImage: "images/majorArcana/17_theStar.jpg",
+  },
+  {
+    cardName: "The Moon",
+    cardImage: "images/majorArcana/18_theMoon.jpg",
+  },
+  {
+    cardName: "The Sun",
+    cardImage: "images/majorArcana/19_theSun.jpg",
+  },
+  {
+    cardName: "Judgment",
+    cardImage: "images/majorArcana/20_Judgment.jpg",
+  },
+  {
+    cardName: "The World",
+    cardImage: "images/majorArcana/21_theWorld.jpg",
+  },
+];
