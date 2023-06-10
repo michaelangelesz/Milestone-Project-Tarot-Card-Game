6569d407cbbd67eb09fcac477363e1eb571cc0f6
michaelangelesz <125311967+michaelangelesz@users.noreply.github.com>
2023-06-10 13:25:42 -0400

    modify images

---
+++ javascript/fullDeck.js
@@ -0,0 +1,149 @@
+const majorArcana = [
+    // Major Arcana
+    {
+      cardName: "The Fool",
+      cardImage: "images/majorArcana/0_theFool.jpg",
+    },
+    {
+      cardName: "The Magician",
+      cardImage: "images/majorArcana/1_theMagician.jpg",
+    },
+    {
+      cardName: "The High Priestess",
+      cardImage: "images/majorArcana/2_theHighPriestess.jpg",
+    },
+    {
+      cardName: "The Empress",
+      cardImage: "images/majorArcana/3_theEmpress.jpg",
+    },
+    {
+      cardName: "The Emperor",
+      cardImage: "images/majorArcana/4_theEmperor.jpg",
+    },
+    {
+      cardName: "The Hierophant",
+      cardImage: "images/majorArcana/5_theHierophant.jpg",
+    },
+    {
+      cardName: "The Lovers",
+      cardImage: "images/majorArcana/6_theLovers.jpg",
+    },
+    {
+      cardName: "The Chariot",
+      cardImage: "images/majorArcana/7_theChariot.jpg",
+    },
+    {
+      cardName: "Strength",
+      cardImage: "images/majorArcana/8_Strength.jpg",
+    },
+    {
+      cardName: "The Hermit",
+      cardImage: "images/majorArcana/9_theHermit.jpg",
+    },
+    {
+      cardName: "Wheel of Fortune",
+      cardImage: "images/majorArcana/10_wheelOfFortune.jpg",
+    },
+    {
+      cardName: "Justice",
+      cardImage: "images/majorArcana/11_Justice.jpg",
+    },
+    {
+      cardName: "The Hanged Man",
+      cardImage: "images/majorArcana/12_theHangedMan.jpg",
+    },
+    {
+      cardName: "Death",
+      cardImage: "images/majorArcana/13_Death.jpg",
+    },
+    {
+      cardName: "Temperance",
+      cardImage: "images/majorArcana/14_Temperance.jpg",
+    },
+    {
+      cardName: "The Devil",
+      cardImage: "images/majorArcana/15_theDevil.jpg",
+    },
+    {
+      cardName: "The Tower",
+      cardImage: "images/majorArcana/16_theTower.jpg",
+    },
+    {
+      cardName: "The Star",
+      cardImage: "images/majorArcana/17_theStar.jpg",
+    },
+    {
+      cardName: "The Moon",
+      cardImage: "images/majorArcana/18_theMoon.jpg",
+    },
+    {
+      cardName: "The Sun",
+      cardImage: "images/majorArcana/19_theSun.jpg",
+    },
+    {
+      cardName: "Judgment",
+      cardImage: "images/majorArcana/20_Judgment.jpg",
+    },
+    {
+      cardName: "The World",
+      cardImage: "images/majorArcana/21_theWorld.jpg",
+    },
+    // Wands
+    {
+        cardName: "Ace of Wands",
+        cardImage: "images/minorArcana/AceofWands.jpg"
+      },
+      {
+        cardName: "Two of Wands",
+        cardImage: "images/minorArcana/TwoofWands.jpg"
+      },
+      {
+        cardName: "Three of Wands",
+        cardImage: "images/minorArcana/ThreeofWands.jpg"
+      },
+      {
+        cardName: "Four of Wands",
+        cardImage: "images/minorArcana/FourofWands.jpg"
+      },
+      {
+        cardName: "Five of Wands",
+        cardImage: "images/minorArcana/FiveofWands.jpg"
+      },
+      {
+        cardName: "Six of Wands",
+        cardImage: "images/minorArcana/SixofWands.jpg"
+      },
+      {
+        cardName: "Seven of Wands",
+        cardImage: "images/minorArcana/SevenofWands.jpg"
+      },
+      {
+        cardName: "Eight of Wands",
+        cardImage: "images/minorArcana/EightofWands.jpg"
+      },
+      {
+        cardName: "Nine of Wands",
+        cardImage: "images/minorArcana/NineofWands.jpg"
+      },
+      {
+        cardName: "Ten of Wands",
+        cardImage: "images/minorArcana/TenofWands.jpg"
+      },
+      {
+        cardName: "Page of Wands",
+        cardImage: "images/minorArcana/PageofWands.jpg"
+      },
+      {
+        cardName: "Knight of Wands",
+        cardImage: "images/minorArcana/KnightofWands.jpg"
+      },
+      {
+        cardName: "Queen of Wands",
+        cardImage: "images/minorArcana/QueenofWands.jpg"
+      },
+      {
+        cardName: "King of Wands",
+        cardImage: "images/minorArcana/KingofWands.jpg"
+      }
+  ];
+  
